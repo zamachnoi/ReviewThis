166a4c1b5d38324f285a3d695e5ea793f794821a
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-01-30 20:58:57 -0500

    Refactor user-related functions and add user handlers

--- server/data/user.go
+++ server/data/user.go
@@ -3,12 +3,23 @@ package data
 
 import (
 	"github.com/zamachnoi/viewthis/db"
-	"github.com/zamachnoi/viewthis/models" // import models
+	"github.com/zamachnoi/viewthis/models"
+	"gorm.io/gorm"
 )
 
-func GetUser(id uint) (*models.User, error) {
-    var user models.User // use models.User
+func GetUserByID(id uint) (*models.User, error) {
+    var user models.User
     if err := db.GetDB().First(&user, id).Error; err != nil {
+        if err == gorm.ErrRecordNotFound {
+            return nil, nil
+        }
+        return nil, err
+    }
+    return &user, nil
+}
+
+func CreateUser(user models.User) (*models.User, error) {
+    if err := db.GetDB().Create(&user).Error; err != nil {
         return nil, err
     }
     return &user, nil
--- server/handlers/user.go
+++ server/handlers/user.go
@@ -1,3 +1,62 @@
+// handlers/user.go
 package handlers
 
-import ()
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/zamachnoi/viewthis/data"
+	"github.com/zamachnoi/viewthis/models"
+)
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case "GET":
+        getUserHandler(w, r)
+    case "POST":
+        createUserHandler(w, r)
+    default:
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    }
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+    idStr := chi.URLParam(r, "id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        http.Error(w, "Invalid user ID", http.StatusBadRequest)
+        return
+    }
+
+    user, err := data.GetUserByID(uint(id))
+    if err != nil {
+        http.Error(w, "Error retrieving user", http.StatusInternalServerError)
+        return
+    }
+
+    if user == nil {
+        http.Error(w, "User not found", http.StatusNotFound)
+        return
+    }
+
+    json.NewEncoder(w).Encode(user)
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+    var newUser models.User
+    err := json.NewDecoder(r.Body).Decode(&newUser)
+    if err != nil {
+        http.Error(w, "Invalid request body", http.StatusBadRequest)
+        return
+    }
+
+    createdUser, err := data.CreateUser(newUser)
+    if err != nil {
+        http.Error(w, "Error creating user", http.StatusInternalServerError)
+        return
+    }
+
+    json.NewEncoder(w).Encode(createdUser)
+}
