d19063a245605d1f17d23498c75f536a249ec553
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-02 06:55:56 -0500

    aaaa

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Hi there! I'm a Next.js app.
+			<p>Hi!</p>
 		</main>
 	)
 }
