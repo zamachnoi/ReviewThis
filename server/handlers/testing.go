d35397d5bc2815022eeec18a305971202afd8f5c
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-02 04:24:33 -0500

    pwease

--- server/handlers/testing.go
+++ server/handlers/testing.go
@@ -13,7 +13,7 @@ type TestingResponse struct {
 
 func TestingHandler(w http.ResponseWriter, r *http.Request) {
 	response := TestingResponse{
-		Message: "Testing action12",
+		Message: "Testing action122",
 	}
 	json.NewEncoder(w).Encode(response)
 }
