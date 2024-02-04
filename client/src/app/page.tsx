172e398201600bfbdbf63a6a1021b69877aa80d3
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-04 00:54:54 -0500

    CHANGE TO THIS!

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image"
 
-export default function Home() {
+// fetch data from api.viewthis.app/api/test
+
+async function getData() {
+	const res = await fetch("https://api.viewthis.app/api/test", {
+		credentials: "include",
+	}) //with credentials: "include"
+	// The return value is *not* serialized
+	// You can return Date, Map, Set, etc.
+
+	if (!res.ok) {
+		console.log(res.status, res.statusText)
+		// This will activate the closest `error.js` Error Boundary
+		// throw new Error("Failed to fetch data")
+		return "Failed to fetch data"
+	}
+
+	return res.json()
+}
+
+export default async function Home() {
+	let data = await getData()
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<p>Hi!</p>
+			<p>{data}</p>
 		</main>
 	)
 }
