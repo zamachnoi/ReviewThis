233e6a1ede1f852c5b568e69944c8d90ad498f51
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-05 00:14:49 -0500

    agane

--- client/src/app/clientUtils.ts
+++ client/src/app/clientUtils.ts
@@ -1,6 +1,3 @@
-"use client"
-import { useCookies } from "next-client-cookies"
-
 export function getClientApiUrl(path: string) {
 	return process.env.NODE_ENV === "development"
 		? "http://127.0.0.1:3001/api" + path
@@ -11,15 +8,15 @@ export function parseJwt(token: string) {
 	return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString())
 }
 
-export function getClientJwt() {
-	const cookies = useCookies()
+// export function getClientJwt() {
+// 	const cookies = useCookies()
 
-	const viewthisJwt = cookies.get("_viewthis_jwt")
+// 	const viewthisJwt = cookies.get("_viewthis_jwt")
 
-	return viewthisJwt ? viewthisJwt : ""
-}
+// 	return viewthisJwt ? viewthisJwt : ""
+// }
 
-export function getClientJwtAndParse() {
-	const jwt = getClientJwt()
-	return jwt ? parseJwt(jwt) : ""
-}
+// export function getClientJwtAndParse() {
+// 	const jwt = getClientJwt()
+// 	return jwt ? parseJwt(jwt) : ""
+// }
