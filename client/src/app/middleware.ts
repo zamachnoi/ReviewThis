532170afbc19a3f3c72093307a847a26c17c431c
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-04 21:08:13 -0500

    yass

--- client/src/app/middleware.ts
+++ client/src/app/middleware.ts
@@ -1,5 +1,5 @@
 export function getApiUrl(path: string) {
 	return process.env.NODE_ENV === "development"
-		? "https://127.0.0.1:3001/api/" + path
+		? "http://127.0.0.1:3001/api/" + path
 		: "https://api.viewthis.app/api/" + path
 }
