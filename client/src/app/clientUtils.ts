c0e90945b3d03213afb051ca36f1f0a6a887631b
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-06 04:44:52 -0500

    remove client cookiesss

--- client/src/app/clientUtils.ts
+++ client/src/app/clientUtils.ts
@@ -10,15 +10,15 @@ export function parseJwt(token: string) {
 	return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString())
 }
 
-export function getClientJwt() {
-	const cookies = useCookies()
+// export function getClientJwt() {
+// 	const cookies = useCookies()
 
-	const viewthisJwt = cookies.get("_viewthis_jwt")
+// 	const viewthisJwt = cookies.get("_viewthis_jwt")
 
-	return viewthisJwt ? viewthisJwt : ""
-}
+// 	return viewthisJwt ? viewthisJwt : ""
+// }
 
-export function getClientJwtAndParse() {
-	const jwt = getClientJwt()
-	return jwt ? parseJwt(jwt) : ""
-}
+// export function getClientJwtAndParse() {
+// 	const jwt = getClientJwt()
+// 	return jwt ? parseJwt(jwt) : ""
+// }
