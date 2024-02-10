bc9c4fbd7700cc4a08dba9d7523b5a1c2262d1cc
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-02-10 13:12:15 -0500

    lint-fix

--- client/src/components/queues/queue-card.tsx
+++ client/src/components/queues/queue-card.tsx
@@ -53,27 +53,32 @@ export default function QueueCard({ queue }: { queue: Queue }) {
 			</CardHeader>
 			<CardContent>
 				<ScrollArea className="w-full p-4 border rounded-md h-[200px]">
-					Jokester began sneaking into the castle in the middle of the
-					night and leaving jokes all over the place: under the king's
-					pillow, in his soup, even in the royal toilet. The king was
-					furious, but he couldn't seem to stop Jokester. And then,
-					one day, the people of the kingdom discovered that the jokes
-					left by Jokester were so funny that they couldn't help but
-					laugh. And once they started laughing, they couldn't stop.
-					Jokester began sneaking into the castle in the middle of the
-					night and leaving jokes all over the place: under the king's
-					pillow, in his soup, even in the royal toilet. The king was
-					furious, but he couldn't seem to stop Jokester. And then,
-					one day, the people of the kingdom discovered that the jokes
-					left by Jokester were so funny that they couldn't help but
-					laugh. And once they started laughing, they couldn't stop.
-					Jokester began sneaking into the castle in the middle of the
-					night and leaving jokes all over the place: under the king's
-					pillow, in his soup, even in the royal toilet. The king was
-					furious, but he couldn't seem to stop Jokester. And then,
-					one day, the people of the kingdom discovered that the jokes
-					left by Jokester were so funny that they couldn't help but
-					laugh. And once they started laughing, they couldn't stop.
+					<p>
+						{`Jokester began sneaking into the castle in the middle
+						of the night and leaving jokes all over the place: under
+						the king's pillow, in his soup, even in the royal
+						toilet. The king was furious, but he couldn't seem to
+						stop Jokester. And then, one day, the people of the
+						kingdom discovered that the jokes left by Jokester were
+						so funny that they couldn't help but laugh. And once
+						they started laughing, they couldn't stop. Jokester
+						began sneaking into the castle in the middle of the
+						night and leaving jokes all over the place: under the
+						king's pillow, in his soup, even in the royal toilet.
+						The king was furious, but he couldn't seem to stop
+						Jokester. And then, one day, the people of the kingdom
+						discovered that the jokes left by Jokester were so funny
+						that they couldn't help but laugh. And once they started
+						laughing, they couldn't stop. Jokester began sneaking
+						into the castle in the middle of the night and leaving
+						jokes all over the place: under the king's pillow, in
+						his soup, even in the royal toilet. The king was
+						furious, but he couldn't seem to stop Jokester. And
+						then, one day, the people of the kingdom discovered that
+						the jokes left by Jokester were so funny that they
+						couldn't help but laugh. And once they started laughing,
+						they couldn't stop.`}
+					</p>
 				</ScrollArea>
 			</CardContent>
 			<CardFooter>
